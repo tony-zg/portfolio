66caf932a5ca541404fdd0ff19f2346a1364971b
Tony Guan <tonyg06@msn.com>
2022-09-12 02:01:30 +1000

    update skill section

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,11 @@
 const Skill = ({ skillImg, skillText, altText }) => {
   return (
     <div className="box-shadow">
-      <img className="w-20 h-20 mx-auto mt-4" src={skillImg} alt={altText} />
+      <img
+        className="w-20 h-20 mx-auto mt-4 rounded-md"
+        src={skillImg}
+        alt={altText}
+      />
       <p className="my-4">{skillText}</p>
     </div>
   );
